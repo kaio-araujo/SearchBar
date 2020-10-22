953af5abe9cb95833fad0527e31de1773980f565
Kaio <kaioaguanais@gmail.com>
2020-10-22 14:44:39 -0700

    Initial Commit

---
+++ SearchBar/SearchBarApp.swift
@@ -0,0 +1,17 @@
+//
+//  SearchBarApp.swift
+//  SearchBar
+//
+//  Created by Kaio Guanais on 2020-10-22.
+//
+
+import SwiftUI
+
+@main
+struct SearchBarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
